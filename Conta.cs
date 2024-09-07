357a5413e7bfcb423f269f23d49ce9e304140381
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-06 23:18:35 -0300

    criada as classes e os métodos

--- Conta.cs
+++ Conta.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReviBank
 {
     public class Conta
     {
+        public string Nome;
+        public string NumeroConta;
+        public double Saldo;
+
+        public Conta(string nome, string numeroConta)
+        {
+            Nome = nome;
+            NumeroConta = numeroConta;
+        }
+
+        public Conta(string nome, string numeroConta, double saldo) : this(nome, numeroConta)
+        {
+            Saldo = saldo;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo -= valor + 5.00;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"{Nome}\n{NumeroConta}\n{Saldo}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Boas vindas ao seu Banco");
+﻿using System;
+using ReviBank;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Boas vindas ao seu Banco");
+        Conta conta01 = new Conta("Luciano", "559", 50000);
+        conta01.ExibirDados();
+    }
+}

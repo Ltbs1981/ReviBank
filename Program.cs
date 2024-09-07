3894a6b700ac3751c9f220c5514762df034fcd39
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-06 23:34:16 -0300

    testecorreção classe conta

--- Program.cs
+++ Program.cs
@@ -8,5 +8,15 @@ class Program
         Console.WriteLine("Boas vindas ao seu Banco");
         Conta conta01 = new Conta("Luciano", "559", 50000);
         conta01.ExibirDados();
+        double depositando = 5000;
+        conta01.Depositar(depositando);
+
+        conta01.ExibirDados();
+
+        conta01.Sacar(55000);
+
+        conta01.ExibirDados();
+
+
     }
 }

2d41147f3ffd76ec77eba99c50d3f46d371f047d
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-03 15:11:04 -0300

    sobrecargas nas classes derivadas

--- ContaPrime.cs
+++ ContaPrime.cs
@@ -2,21 +2,44 @@
 
 namespace ReviBank
 {
-    public class ContaPrime : Conta
+    public sealed class ContaPrime : Conta
     {
         public int PontosEmMilhas { get; set; }
         public double ChequeEspecial { get; set; }
 
+        public override string Nome { get; set; }
+        public override string NumeroConta { get; protected set; }
+        public override double Saldo { get; protected set; }
+
         public ContaPrime(string nome, string numeroConta, int pontosEmMilhas) : base(nome, numeroConta)
         {
             PontosEmMilhas = pontosEmMilhas;
-            ChequeEspecial = 100000; 
+            ChequeEspecial = 100000;
+            Saldo = 0;
         }
 
         public ContaPrime(string nome, string numeroConta, double saldo, int pontosEmMilhas) : base(nome, numeroConta, saldo)
         {
             PontosEmMilhas = pontosEmMilhas;
-            ChequeEspecial = 100000; 
+            ChequeEspecial = 100000;
+        }
+
+        public override void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public override void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+            else
+            {
+                Console.WriteLine($"O valor de R$ {valor} foi retirado da sua conta");
+                Saldo -= valor + 20.00;
+            }
         }
 
         public override string ToString()
@@ -25,3 +48,4 @@ namespace ReviBank
         }
     }
 }
+
--- ContaSalario.cs
+++ ContaSalario.cs
@@ -2,21 +2,44 @@
 
 namespace ReviBank
 {
-    public class ContaSalario : Conta
+    public sealed class ContaSalario : Conta
     {
         public string NomeEmpresa { get; set; }
         public double Emprestimo { get; set; }
+
+        public override string Nome { get; set; }
+        public override string NumeroConta { get; protected set; }
+        public override double Saldo { get; protected set; }
 
         public ContaSalario(string nome, string numeroConta, string nomeEmpresa) : base(nome, numeroConta)
         {
             NomeEmpresa = nomeEmpresa;
             Emprestimo = 10000;
+            Saldo = 0;
         }
 
         public ContaSalario(string nome, string numeroConta, double saldo, string nomeEmpresa) : base(nome, numeroConta, saldo)
         {
             NomeEmpresa = nomeEmpresa;
             Emprestimo = 10000;
+        }
+
+        public override void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public override void Sacar(double valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para saque.");
+            }
+            else
+            {
+                Console.WriteLine($"O valor de R$ {valor} foi retirado da sua conta");
+                Saldo -= valor + 5.00;
+            }
         }
 
         public override string ToString()
